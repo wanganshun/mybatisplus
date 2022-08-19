5b980f0cbe6156bab2d6244a4236a09ad21b2a66
wanganshun <2635550416@qq.com>
2022-08-19 18:02:18 +0800

    test

---
+++ src/test/java/cn/was/test/Test01.java
@@ -0,0 +1,13 @@
+package cn.was.test;
+
+/**
+ * @program: mybatisplus01
+ * @author: WangAnShun
+ * @create: 2022-08-19 17:51
+ **/
+public class Test01 {
+    public static void main(String[] args) {
+        int a=2<<2;
+        System.out.println(a);
+    }
+}
