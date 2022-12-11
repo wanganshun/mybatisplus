10638ab34e628a39e7f5355045710e2818439e22
wanganshun <2635550416@qq.com>
2022-12-12 00:05:30 +0800

    test

---
+++ src/test/java/cn/was/test/Node.java
@@ -0,0 +1,16 @@
+package cn.was.test;
+
+/**
+ * @program: mybatisplus01
+ * @author: WangAnShun
+ * @create: 2022-12-11 23:31
+ **/
+public class Node {
+    public int data;
+    public Node leftNode;
+    public Node rightNode;
+    public Node(int key){
+        data=key;
+
+    }
+}
